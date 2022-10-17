305625f4030ab4376e2323ac3e9be5df995b4841
Mahesh Prasanna <damp.dassanayake@gmail.com>
2022-10-17 14:40:13 +0530

    New student button set

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-console.log('Hiii');
+const tblStudent = document.getElementById('tbl-student');
+const btnNew = document.getElementById('btn-new');
+const btnSave = document.getElementById('btn-save');
+const btnClear = document.getElementById('btn-clear');
+const txtId = document.getElementById('txt-id');
+const txtName = document.getElementById('txt-name');
+const txtConatct = document.getElementById('txt-contact');
+const txtAddress = document.getElementById('txt-address');
+
+btnNew.addEventListener('click', () =>{
+    [txtId, txtName, txtAddress, txtConatct, btnSave, btnClear]
+    .forEach(ctrl => ctrl.disabled = false);
+    txtId.value = generateNewStudentId();
+    txtName.focus();
+});
+function generateNewStudentId(){
+    return "S001";
+}
